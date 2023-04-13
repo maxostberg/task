63bf510fec76cd3bd3eda3537762e2ba1469ab55
Max Östberg <maxostberg97@gmail.com>
2023-04-14 01:00:45 +0200

    impl boltdb create getAll

--- cmd/do.go
+++ cmd/do.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -10,7 +11,18 @@ var doCmd = &cobra.Command{
 	Use:   "do",
 	Short: "Mark a task as completed with a number",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(cmd.Args)
+		var taskIds []int
+
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Printf("Argument \"%s\" is not a number\n", arg)
+				continue
+			}
+			taskIds = append(taskIds, id)
+		}
+
+		fmt.Println(taskIds)
 	},
 }
 
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/maxostberg/task/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/maxostberg/task/cmd"
+	"github.com/maxostberg/task/db"
+)
 
 func main() {
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("Could not find home directory: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	dbPath := filepath.Join(dir, "tasks.db")
+	err = db.Init(dbPath)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
 	cmd.Execute()
 }
